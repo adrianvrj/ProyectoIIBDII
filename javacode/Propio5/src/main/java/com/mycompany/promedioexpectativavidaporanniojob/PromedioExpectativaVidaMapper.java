e88997b9157e95d4f3acd6225e2264cac552a8e4
Mariana <mfernandezmar3@gmail.com>
2023-06-04 00:38:08 -0600

    added propio5 job

---
+++ javacode/Propio5/src/main/java/com/mycompany/promedioexpectativavidaporanniojob/PromedioExpectativaVidaMapper.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.promedioexpectativavidaporanniojob;
+
+import java.io.IOException;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class PromedioExpectativaVidaMapper extends Mapper<Object, Text, Text, FloatWritable> {
+    private final Text year = new Text();
+    private final FloatWritable count = new FloatWritable();
+    private boolean isFirstLine = true;
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        if (isFirstLine) {
+            isFirstLine = false;
+            return; // Skip the first line
+        }
+        String[] fields = value.toString().split(",");
+        String yearData = fields[1];
+        String valstr = "";
+        for(int i = 1; fields[3].charAt(i) != '"'; i++){
+            valstr += fields[3].charAt(i);
+        }
+        float val = Float.parseFloat(valstr);
+
+        year.set(yearData);
+        count.set(val);
+
+        context.write(year, count);
+    }
+}
